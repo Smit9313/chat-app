1ca7868bbcdb3785f296d6ba5a81c44fc7cd8b72
Smit9313 <smitdudhat10@gmail.com>
2023-05-26 12:21:38 +0530

    f6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
-import { Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ChatPage from './pages/ChatPage';
+import "./App.css";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
   return (
-    <div className='app'>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path="/chats" component={ChatPage}/>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/chats" component={ChatPage} />
+      </Switch>
     </div>
   );
 }
