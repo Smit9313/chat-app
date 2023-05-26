73e9b2062aabadf7896e4c54049fdd9f948e27dc
Smit9313 <smitdudhat10@gmail.com>
2023-05-26 13:35:53 +0530

    f10

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,20 +1,34 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
 import MyChats from "../components/MyChats";
 import ChatBox from "../components/ChatBox";
 import { ChatState } from "../Context/ChatProvider";
 import { Box } from "@chakra-ui/react";
+import { isEmpty } from "lodash";
+import { useHistory } from "react-router-dom";
 
 const ChatPage = () => {
   const { user } = ChatState();
-  const [fetchAgain, setFetchAgain ] = useState(false);
+  const [fetchAgain, setFetchAgain] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (isEmpty(user)) history.push("/");
+  }, [history, user]);
+
   return (
     <div style={{ width: "100%" }}>
       {user && <SideDrawer />}
-      <Box w="100%" h="91.5vh" p="10px" 
-      style={{display:"flex", justifyContent: "space-between"}}>
+      <Box
+        w="100%"
+        h="91.5vh"
+        p="10px"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
         {user && <MyChats fetchAgain={fetchAgain} />}
-        {user && <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />}
+        {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
       </Box>
     </div>
   );
