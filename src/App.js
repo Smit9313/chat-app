fdc2d02ed59497eb8d650cddc1c040c5834f8a14
Smit9313 <smitdudhat10@gmail.com>
2023-05-26 12:56:08 +0530

    f8

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+// import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ChatPage from "./pages/ChatPage";
+import { isEmpty } from "lodash";
 
 function App() {
+
+  const user = JSON.parse(localStorage.getItem("userInfo"));
+
   return (
     <div className="app">
-      <Switch>
+      {/* <Switch>
         <Route exact path="/chats" component={ChatPage} />
         <Route exact path="/" component={HomePage} />
-      </Switch>
+      </Switch> */}
+      {isEmpty(user) ? <HomePage/> : <ChatPage/>}
     </div>
   );
 }
