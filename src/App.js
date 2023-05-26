6246fc8c23820d42cc1779e8af40f5efcb43268a
Smit9313 <smitdudhat10@gmail.com>
2023-05-26 13:19:28 +0530

    f9

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useHistory } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ChatPage from "./pages/ChatPage";
+import { ChatState } from "./Context/ChatProvider";
+import { useEffect } from "react";
 
 function App() {
+  const { user } = ChatState();
+  const history = useHistory();
+  useEffect(() => {
+    if (user) history.push("/chats");
+  }, [user, history]);
   return (
     <div className="app">
       <Route exact path="/" component={HomePage} />
